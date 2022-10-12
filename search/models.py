c34488592b5fe69e59a1ee5d194cc4bdcdcafe77
matthew28302 <20521508@gm.uit.edu.vn>
2022-10-12 11:41:44 +0700

    add models

--- search/models.py
+++ search/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 # Create your models here.
-class Account(models.MODEL):
+class Account(models.Model):
     username = models.CharField(("Tên đăng nhập"), max_length=50)
-    password = models.CharField(widget = forms.PasswordInput)
-    fullname = models.CharField("Họ tên", max_leng=100)
+    password = models.CharField("Mật khẩu", max_length=50)
+    fullname = models.CharField("Họ tên", max_length=100)
     email = models.EmailField(("Email"), max_length=254)
-class Item(models.MODEL):
+class Item(models.Model):
     title = models.CharField("Tiêu đề", max_length = 200)
     typeInfo = models.CharField("Loại tin",max_length = 50) # tin : tìm kiếm / thất lạc
     typeItem = models.CharField("Phân loại đồ", max_length = 50)
@@ -16,12 +16,12 @@ class Item(models.MODEL):
     address = models.CharField("Địa chỉ", max_length = 200)
     phoneNum = models.CharField("Số điện thoại", max_length = 10)
     email = models.EmailField(("Email"), max_length=254)
-class Message(models.MODEL):
+class Message(models.Model):
     fullname = models.CharField(("Họ tên"), max_length=100)
     content = models.CharField(("Nội dung"), max_length=50)
     time = models.CharField(("Thời gian"), max_length=50)
     status = models.CharField("Trạng thái tin nhắn",max_length=50) #Success/Fail
-class menuItem(models.MODEL):
+class menuItem(models.Model):
     nameItem = models.CharField("Tên kiểu đồ",max_length=50)
     amountLost = models.IntegerField(("Số lượng mất"))
     amountPick = models.IntegerField("Số lượng nhặt được")
