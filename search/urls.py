1b9bc7919276b668a24500b8eff32fbe369e25a1
20521143 <20521143@gm.uit.edu.vn>
2022-10-18 21:13:30 +0700

    add new project

--- search/urls.py
+++ search/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.hello, name="hello"),
-    path("<str:name>", views.greet, name="greet")
+    # path("<str:name>", views.greet, name="greet"),
+    path("login", views.login, name="login"),
+    path("register", views.register, name="register"),
+    path("resetpwd", views.resetpwd, name="resetpwd")
 ]
--- search/views.py
+++ search/views.py
@@ -1,9 +1,22 @@
+from unittest import loader
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
 
 # Create your views here.
 def greet(request, name):
     return HttpResponse(f"Hello {name}")
 
 def hello(request):
-    return HttpResponse("hello world")
+    template = loader.get_template('search/register.html')
+    return  HttpResponse(template.render(request))
+
+
+def login(request):
+    return render(request, 'search/login.html')
+
+def resetpwd(request):
+    return render(request,'search/reset_password.html')
+
+def register(request):
+    return render(request,'search/register.html')
